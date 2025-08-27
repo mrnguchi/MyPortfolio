55f5d8370e49fbbdc133ac2ad27fb0cf3975b966
mrnguchi <munohnguchi334@gmail.com>
2025-08-27 04:31:39 +0100

    Header problem fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 // filepath: src/app/layout.tsx
 import "./globals.css";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
